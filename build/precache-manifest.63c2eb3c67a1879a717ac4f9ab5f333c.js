ed3d503a5a0e3e27cb2a49a7e243568bd18a2f63
Tor Berre <tor0405@hotmail.no>
2020-05-18 19:38:21 +0200

    fix bug

---
+++ build/precache-manifest.63c2eb3c67a1879a717ac4f9ab5f333c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b150d41d4e031abaed4527c6f5c93830",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e13b27340f68ba89aee",
+    "url": "/static/css/2.24137ca6.chunk.css"
+  },
+  {
+    "revision": "5abb0316b4652dc68617",
+    "url": "/static/css/main.e4124129.chunk.css"
+  },
+  {
+    "revision": "0e13b27340f68ba89aee",
+    "url": "/static/js/2.ec6e9506.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.ec6e9506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abb0316b4652dc68617",
+    "url": "/static/js/main.2bce5722.chunk.js"
+  },
+  {
+    "revision": "77aeb331390d0c19b8e3",
+    "url": "/static/js/runtime-main.a74e1163.js"
+  }
+]);
