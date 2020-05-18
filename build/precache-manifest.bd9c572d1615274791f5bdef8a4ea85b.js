c702065a6b7777c2f5026b96b49d7d8f411fca4f
Tor Berre <tor0405@hotmail.no>
2020-05-18 16:09:57 +0200

    new layout

---
+++ build/precache-manifest.bd9c572d1615274791f5bdef8a4ea85b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57269b124885f5f9a600b6a8575374b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e13b27340f68ba89aee",
+    "url": "/static/css/2.24137ca6.chunk.css"
+  },
+  {
+    "revision": "f0134bf77268c607336b",
+    "url": "/static/css/main.0e7214be.chunk.css"
+  },
+  {
+    "revision": "0e13b27340f68ba89aee",
+    "url": "/static/js/2.ec6e9506.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.ec6e9506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0134bf77268c607336b",
+    "url": "/static/js/main.c8703b82.chunk.js"
+  },
+  {
+    "revision": "77aeb331390d0c19b8e3",
+    "url": "/static/js/runtime-main.a74e1163.js"
+  }
+]);
